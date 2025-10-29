2cac3ac1aa5538157854034414107368eb6278f2
dragonstonehafiz <dragonstonehafiz@gmail.com>
2025-10-29 09:30:19 +0800

    added new images for projects, removed # from links

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,14 @@ import 'routes/app_routes.dart';
 import 'utils/theme.dart';
 import 'utils/page_collection.dart';
 import 'utils/project_collection.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() async {
   // Ensure Flutter bindings are initialized before using rootBundle or other
   // services. This prevents the 'Binding has not yet been initialized' error
   // when loading assets during startup.
   WidgetsFlutterBinding.ensureInitialized();
+  usePathUrlStrategy();
 
   // Initialize singletons from assets and dynamic routes
   await Future.wait([
